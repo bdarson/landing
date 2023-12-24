18642352b39cecffd983ce1c6f1e704a02663fc6
bdarson <147023762+bdarson@users.noreply.github.com>
2023-12-24 04:01:18 -0600

    Commenting
    Made comments in the jQuery to make sure I was understanding what it was doing

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
-$(document).ready(function() {
-$('#navigation li a').hover(
+$(document).ready(function() { //initializes the script
+$('#navigation li a').hover( //triggers the code below on hover
     function(){
         //we get our current filename and use it for the src
-        var linkIndex = $(this).attr("data-filename");
+        var linkIndex = $(this).attr("data-filename"); //sets the linkIndex variable so it knows which image to grab - data-filename is a custom attribute
         $('.right-sec-upper img')
-            .fadeOut(200, function() {
+            .fadeOut(200, function() { //makes the image switch a function of the fadeout, hiding it
                 $('.right-sec-upper img').attr('src', './images/'+linkIndex+'.jpg')
             })
-            .fadeIn(200);
+            .fadeIn(200); //fades the new image in
     },
     function(){
-        $('.right-sec-upper img').fadeOut(200);
+        $('.right-sec-upper img').fadeOut(200); //fades the old image out when mouseOff is triggered
     }
 );
 });
